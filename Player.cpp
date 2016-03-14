9b63f568b9948124412b82265b77827746bc5300
NortonGitHub <yuuki_vs_ninjin@outlook.jp>
2016-03-14 23:38:53 +0900

    [編集]キャラ死亡を追加

--- Player.cpp
+++ Player.cpp
@@ -2,52 +2,26 @@
 
 
 
-Player::Player()
+Player::Player(int n)
+	:font(30)
 {
 	_wr = 40;
 	_hr = 60;
 
+	_hp = 100;
+
 	_accelerationX = 0.0;
 	_accelerationY = 0.0;
-	
+
 	color = Palette::White;
 
 	frame = 0;
-	
+
+	initPlayer(n);
+
+	_direction = "stop";
 }
 
-
-Player::~Player()
-{
-}
-
-void Player::update()
-{
-	updateResistance();
-
-	_x += _accelerationX;
-	_y += _accelerationY;
-
-	Body.x = _x;
-	Body.y = _y;
-
-	key.inputKey(_accelerationX, _accelerationY,num);
-
-	rollDirection();
-
-	++frame;
-}
-
-void Player::draw()
-{
-	Body.draw(color);
-}
-
-void Player::setAcceleration(double _x, double _y)
-{
-	_accelerationX = _x;
-	_accelerationY = _y;
-}
 
 void Player::initPlayer(int n)
 {
@@ -56,6 +30,80 @@ void Player::initPlayer(int n)
 	_y = (num == 1) ? 380 : 82;
 
 	Body.set(_x, _y, _wr / 2, _hr / 2);
+}
+
+bool Player::isHPFallBelowZERO()
+{
+	if (_hp <= 0) {
+		return true;
+	}
+	return false;
+}
+
+
+Player::~Player()
+{
+}
+
+Player* Player::update()
+{
+	updateResistance();
+
+	_x += _accelerationX;
+	_y += _accelerationY;
+
+	setMovingDirection();
+
+	Body.x = _x;
+	Body.y = _y;
+
+	if (isHPFallBelowZERO()) {
+		return this;
+	}
+
+	key.inputKey(_accelerationX, _accelerationY, num, _direction);
+
+	rollDirection();
+
+	effects.update();
+
+
+	if (frame >= 1000) {
+		frame = 0;
+	}
+	++frame;
+
+	return this;
+}
+
+void Player::draw()
+{
+	(!isHPFallBelowZERO()) ? drawNormal() : drawBroken();
+}
+
+void Player::drawNormal()
+{
+	Body.draw(color);
+
+	effects.draw();
+
+	font(L"‰Á‘¬“x", _accelerationX, L"HP", _hp).draw(20.0, 32.0*num);
+}
+
+void Player::drawBroken()
+{
+}
+
+void Player::setAcceleration(double _x, double _y)
+{
+	_accelerationX = _x;
+	_accelerationY = _y;
+	
+}
+
+void Player::setEffectsInit(Point oponentAcce, Vec2 myCenter)
+{
+	effects.init(oponentAcce, myCenter, _direction);
 }
 
 void Player::updateResistance()
@@ -93,7 +141,7 @@ void Player::rollDirection()
 	if (_accelerationY != 0.0) {
 		graPeriod = fmod(graPeriod + 20, 360.0);
 		double rad = Radians(graPeriod);
-		double size = sin(rad);
+		double size = cos(rad);
 
 		double a = (_hr / 2) - size*4;
 
@@ -107,4 +155,41 @@ void Player::rollDirection()
 		Body.y += bi;
 
 	}
+}
+
+void Player::setMovingDirection()
+{
+	if (_accelerationX > 0.0) {
+		_direction = "right";
+	}
+	else if (_accelerationX < 0.0) {
+		_direction = "left";
+	}
+	else if (_accelerationY > 0.0) {
+		_direction = "down";
+	}
+	else if (_accelerationY < 0.0) {
+		_direction = "up";
+	}
+	else {
+		_direction = "stop";
+	}
+}
+
+void Player::reflectingDamageToHP(Point opponent)
+{
+	double _acceSum1 = abs(_accelerationX + _accelerationY);
+	double _acceSum2 = abs(opponent.x + opponent.y);
+
+
+	double _damage = (_acceSum1 > _acceSum2) ? _acceSum2 / 2 : _acceSum2 * 1.5;
+	_hp -= _damage;
+}
+
+void Player::reflectingDamageToHPWithWall()
+{
+	double _acceSum = abs(_accelerationX + _accelerationY);
+
+	double _damage = _acceSum;
+	_hp -= _damage;
 }
--- Player.h
+++ Player.h
@@ -1,18 +1,24 @@
 #pragma once
 #include "CharaBase.h"
 #include "KeyBase.h"
+#include"EffectManager.h"
 
 class Player :
 	public CharaBase
 {
 public:
-	Player();
+	Player(int n);
 	~Player();
 
-	void update();
+	Player* update();
 	void draw();
 
+	void drawNormal();
+	void drawBroken();
+
 	void setAcceleration(double _x, double _y);
+
+	void setEffectsInit(Point oponentAcce, Vec2 myCenter);
 
 	int getHeightRadius() { return _hr; };
 	int getWidthRadius() { return _wr; };
@@ -20,20 +26,36 @@ public:
 	double getAccelerationX() { return _accelerationX; }
 	double getAccelerationY() { return _accelerationY; }
 
+	void reflectingDamageToHP(Point opponent);
+	void reflectingDamageToHPWithWall();
+
 	void initPlayer(int n);
 
-	Ellipse Body;
+	Ellipse getEllipseBody() { return Body; };
 
-	std::string getMovingDirection() { return key.keycode; }
+	std::string getMovingDirection() { return _direction; }
+
+	bool isHPFallBelowZERO();
 
 private:
-	KeyBase key;
-	void updateResistance();
 
+
+	Ellipse Body;
+	KeyBase key;
+
+	EffectManager effects;
+
+	void updateResistance();
 	void rollDirection();
+	void setMovingDirection();
 
 	int frame;
 
 	int num;
+	int _hp;
+
+	Font font;	//テスト用
+
+	std::string _direction;//"right,left,up,down,stop"
 };
 
